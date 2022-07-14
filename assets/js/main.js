96381aa85c0b9027220f0edc042ea384fc03f382
Sivagajan <sivagajan@gmail.com>
2022-07-14 13:40:24 +0200

    firsst commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+
+
+/* var z = 34
+z = 67
+console.log(z) */
+
+let x = 34
+x = 67
+console.log(x)
+
+/* const y = 34
+y = 67      // Uncaught TypeError: Assignment to constant variable.
+            // at main.js:12:3   ==> ein CONST kann nicht überschrieben und bei deklarieren muss der ///// Wert direkt übergeben werden 
+console.log(y) */
